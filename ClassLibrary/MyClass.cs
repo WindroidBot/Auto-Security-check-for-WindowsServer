9ab0e70dc1606c083ab13c67ba91e6139d34d451
Windroid <Windroid@126.com>
2016-11-21 23:17:13 +0800

    删除了不必要的using

--- ClassLibrary/MyClass.cs
+++ ClassLibrary/MyClass.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClassLibrary
 {
@@ -10,7 +6,6 @@ namespace ClassLibrary
     {
         public MyClass()
         {
-
         }
         public void DemoDLL()
         {
