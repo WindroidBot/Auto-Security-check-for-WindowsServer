8a57493ee15493100ba51328369bf0db53551d25
Windroid <Windroid@126.com>
2016-11-21 11:10:53 +0800

    测试了创建和引用dll动态链接库

---
+++ ClassLibrary/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class MyClass
+    {
+        public MyClass()
+        {
+
+        }
+        public void DemoDLL()
+        {
+            Console.WriteLine("调用了dll动态链接库");
+        }
+    }
+}
---
+++ TEST_DemoDLL/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace TEST_DemoDLL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass test = new MyClass();
+            test.DemoDLL();
+            Console.ReadLine();
+        }
+    }
+}
